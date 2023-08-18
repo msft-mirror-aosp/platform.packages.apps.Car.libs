0c197fad646e6210a8b698a2e7a16a35c35e3e17
John Patterson <jdp@google.com>
2023-08-17 18:09:23 -0700

    Add a key for the target app component name to CarVoiceInteractionService.
    Test: Still compiles. No behavior changes.
    Change-Id: I3f34068d302bcb008e54674cbf02401c528f20ad

--- car-assist-lib/src/com/android/car/assist/CarVoiceInteractionSession.java
+++ car-assist-lib/src/com/android/car/assist/CarVoiceInteractionSession.java
@@ -72,6 +72,50 @@ public abstract class CarVoiceInteractionSession extends VoiceInteractionSession
      */
     public static final String KEY_CONVERSATION = "KEY_CONVERSATION";
 
+    /* Recipient's phone number. If this and the recipient name are not provided,
+     * by the application, digital assistant must do contact disambiguation
+     * and add the phone number to the pending intent
+     */
+    public static final String KEY_PHONE_NUMBER = "KEY_PHONE_NUMBER";
+
+    /**
+     * Recipient's name. If this and the recipient phone number are not provided by the application,
+     * digital assistant must do contact disambiguation but is not required to add the name to the
+     * PendingIntent.
+     */
+    public static final String KEY_RECIPIENT_NAME = "KEY RECIPIENT NAME";
+
+    /* Recipient's UID in the Contact Provider database. Optionally provided by the application.
+     * Not required to be sent back by the digital assistant.
+     */
+    public static final String KEY_RECIPIENT_UID = "KEY_RECIPIENT_UID";
+
+    /* Friendly name of the device in which to send the message from. If not
+     * provided by the application, digital assistant must do device disambiguation
+     * but is not required to add it to the Pending Intent.
+     */
+    public static final String KEY_DEVICE_NAME = "KEY DEVICE_NAME";
+
+    /* Bluetooth device address of the device of which to send the message from.
+     * If not provided by the application, assistant must do device disambiguation
+     * and add this to the Pendingintent.
+     */
+    public static final String KEY_DEVICE_ADDRESS = "KEY_DEVICE_ADDRESS";
+
+    /* KEY_SEND_PENDING_INTENT is not null and always be provided by the application.
+     * The application must preload the pending intent with any KEYs, it provides the assistant
+     * that is also needed to send the message.
+     * (i.e. if the application passes in the KEY_PHONE_NUMBER in the Bundle,
+     * the assistant can assume the application has already put this in the
+     * PendingIntent and may not re-add it to the PendingIntent).
+     */
+    public static final String KEY_SEND_PENDING_INTENT = "KEY_SEND_PENDING INTENT";
+
+    /**
+     * The key used for the {@link android.content.ComponentName} of the target.
+     */
+    public static final String KEY_TARGET_COMPONENT = "KEY_TARGET_COMPONENT";
+
     /** Indicates to assistant that no action was specified. */
     public static final String VOICE_ACTION_NO_ACTION = "VOICE_ACTION_NO_ACTION";
 
@@ -113,45 +157,6 @@ public abstract class CarVoiceInteractionSession extends VoiceInteractionSession
      * can actually send the SMS.
      */
     public static final String VOICE_ACTION_SEND_SMS = "VOICE_ACTION_SEND_SMS";
-
-    /* Recipient's phone number. If this and the recipient name are not provided,
-     * by the application, digital assistant must do contact disambiguation
-     * and add the phone number to the pending intent
-     */
-    public static final String KEY_PHONE_NUMBER = "KEY_PHONE_NUMBER";
-
-    /**
-     * Recipient's name. If this and the recipient phone number are not provided by the application,
-     * digital assistant must do contact disambiguation but is not required to add the name to the
-     * PendingIntent.
-     */
-    public static final String KEY_RECIPIENT_NAME = "KEY RECIPIENT NAME";
-
-    /* Recipient's UID in the Contact Provider database. Optionally provided by the application.
-     * Not required to be sent back by the digital assistant.
-     */
-    public static final String KEY_RECIPIENT_UID = "KEY_RECIPIENT_UID";
-
-    /* Friendly name of the device in which to send the message from. If not
-     * provided by the application, digital assistant must do device disambiguation
-     * but is not required to add it to the Pending Intent.
-     */
-    public static final String KEY_DEVICE_NAME = "KEY DEVICE_NAME";
-
-    /* Bluetooth device address of the device of which to send the message from.
-     * If not provided by the application, assistant must do device disambiguation
-     * and add this to the Pendingintent.
-     */
-    public static final String KEY_DEVICE_ADDRESS = "KEY_DEVICE_ADDRESS";
-
-    /* KEY_SEND_PENDING_INTENT is not null and always be provided by the application.
-     * The application must preload the pending intent with any KEYs, it provides the assistant
-     * that is also needed to send the message.
-     * (i.e. if the application passes in the KEY_PHONE_NUMBER in the Bundle,
-     * the assistant can assume the application has already put this in the
-     * PendingIntent and may not re-add it to the PendingIntent).
-     */
-    public static final String KEY_SEND_PENDING_INTENT = "KEY_SEND_PENDING INTENT";
 
     /**
      * Indicates to assistant that it should resolve the exception in the given payload (found in
