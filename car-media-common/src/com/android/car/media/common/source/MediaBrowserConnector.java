46dd74341823e8079d895d952dafe9fda11b6a27
Mark Stanford <markstanford@google.com>
2024-01-25 19:25:23 +0000

    Refactor analytics from broadcast receiver to custom action
    Bug: 316228721
    Test: manual; TestMediaApp
    Change-Id: I382182175114d60805acb22933c07586d71d36ac

--- car-media-common/src/com/android/car/media/common/source/MediaBrowserConnector.java
+++ car-media-common/src/com/android/car/media/common/source/MediaBrowserConnector.java
@@ -111,7 +111,7 @@ public class MediaBrowserConnector {
             if (browser.isConnected() && browser.getExtras() != null) {
                 mRootExtras.putAll(browser.getExtras());
             }
-            mAnalyticsManager = AnalyticsHelper.makeAnalyticsManager(mContext, mMediaSource,
+            mAnalyticsManager = AnalyticsHelper.makeAnalyticsManager(mContext, browser,
                     mRootExtras);
         }
 
@@ -120,7 +120,7 @@ public class MediaBrowserConnector {
             mRootExtras.clear();
             mRootExtras.putAll(rootExtras);
             mAnalyticsManager.sendQueue();
-            mAnalyticsManager = AnalyticsHelper.makeAnalyticsManager(mContext, mMediaSource,
+            mAnalyticsManager = AnalyticsHelper.makeAnalyticsManager(mContext, mBrowser,
                     mRootExtras);
         }
 
@@ -253,6 +253,11 @@ public class MediaBrowserConnector {
                 Log.d(TAG, "Disconnecting: " + getSourcePackage()
                         + " mBrowser: " + idHash(mBrowser));
             }
+
+            // Send queued analytic events before we disconnect.
+            Bundle rootExtras =  mBrowser.getExtras() == null ? new Bundle() : mBrowser.getExtras();
+            AnalyticsHelper.makeAnalyticsManager(mContext, mBrowser, rootExtras).sendQueue();
+
             sendNewState(ConnectionStatus.DISCONNECTING);
             mBrowser.disconnect();
         }
