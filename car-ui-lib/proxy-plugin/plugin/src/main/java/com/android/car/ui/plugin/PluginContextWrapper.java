2ac7784d5ed54962b3804fa23b29f517b54a23cc
Ram Parameswaran <rampara@google.com>
2024-04-03 18:05:45 +0000

    Wrap app activity context for plugin UI context
    Fixes: 332716845
    Test: Manual
    
    Change-Id: Ie9d05f0cf76aca7915ceaf610ea2a093d1ba7b1c

--- car-ui-lib/proxy-plugin/plugin/src/main/java/com/android/car/ui/plugin/PluginContextWrapper.java
+++ car-ui-lib/proxy-plugin/plugin/src/main/java/com/android/car/ui/plugin/PluginContextWrapper.java
@@ -19,7 +19,6 @@ package com.android.car.ui.plugin;
 import android.content.Context;
 import android.content.ContextWrapper;
 import android.view.LayoutInflater;
-import android.view.WindowManager;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -31,8 +30,6 @@ public class PluginContextWrapper extends ContextWrapper {
 
     @NonNull
     private final String mApplicationPackageName;
-    @Nullable
-    private WindowManager mWindowManager;
     @Nullable
     private LayoutInflater mLayoutInflater = null;
 
@@ -60,20 +57,8 @@ public class PluginContextWrapper extends ContextWrapper {
         return mApplicationPackageName;
     }
 
-    /**
-     * Sets the {@link WindowManager} instance for the plugin {@link Context}.
-     * This was specifically needed for launching {@code Dialog}s.
-     * @param windowManager needs to be attached to the main Activity
-     */
-    public void setWindowManager(@Nullable WindowManager windowManager) {
-        mWindowManager = windowManager;
-    }
-
     @Override
     public Object getSystemService(@NonNull String name) {
-        if (WINDOW_SERVICE.equals(name) && mWindowManager != null) {
-            return mWindowManager;
-        }
         // Return a layout inflater cloned with this context so that views inflated with a
         // LayoutInflater.from call will return a PluginContextWrapper which is needed to override
         // other methods in Context, e.g., getPackageName
