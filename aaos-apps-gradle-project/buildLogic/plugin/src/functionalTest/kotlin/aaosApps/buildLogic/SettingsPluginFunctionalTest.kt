b311ff130e1e46a24e11357904b21b3436a81fa6
froeht <froeht@google.com>
2024-10-01 19:31:30 +0000

    Fix buildLogic test failure
    The test checked to see if the build directory was in the right place, but it was incorrectly trying to put it in the root directory (`/`)
    
    I've had the test create a dummy .supermanifest file so that the plugin will find it and set the build directory to be relative to the file, as the plugin is supposed to do.
    
    Test: ./gradlew :buildLogic:plugin:test
    Change-Id: I2468e18a395f56bbf517905ddc475d50b640460d

--- aaos-apps-gradle-project/buildLogic/plugin/src/functionalTest/kotlin/aaosApps/buildLogic/SettingsPluginFunctionalTest.kt
+++ aaos-apps-gradle-project/buildLogic/plugin/src/functionalTest/kotlin/aaosApps/buildLogic/SettingsPluginFunctionalTest.kt
@@ -24,37 +24,39 @@ import org.junit.jupiter.api.io.TempDir
 
 class SettingsPluginFunctionalTest {
 
-    @field:TempDir lateinit var projectDir: File
+  @field:TempDir lateinit var projectDir: File
 
-    private val buildFile by lazy { projectDir.resolve("build.gradle.kts") }
-    private val settingsFile by lazy { projectDir.resolve("settings.gradle.kts") }
+  private val buildFile by lazy { projectDir.resolve("build.gradle.kts") }
+  private val settingsFile by lazy { projectDir.resolve("settings.gradle.kts") }
+  // Gotta give the test a root dir to find so that the plugin doesn't fall back to using
+  // "../../../../.."
+  private val supermanFile by lazy { projectDir.resolve(".supermanifest") }
 
-    @Test
-    fun buildDirSet() {
-        // Set up the test build
-        settingsFile.writeText(
-            """
+  @Test
+  fun buildDirSet() {
+    // Set up the test build
+    settingsFile.writeText(
+        """
             plugins {
                 id("aaosApps.buildLogic.settings")
             }
         """
-                .trimIndent()
-        )
-        buildFile.writeText(
-            """
+            .trimIndent())
+    buildFile.writeText(
+        """
             println("My buildDir is " + project.layout.buildDirectory.get().toString())
         """
-                .trimIndent()
-        )
+            .trimIndent())
+    supermanFile.writeText("PLACEHOLDER")
 
-        // Run the build
-        val runner = GradleRunner.create()
-        runner.forwardOutput()
-        runner.withPluginClasspath()
-        runner.withProjectDir(projectDir)
-        val result = runner.build()
+    // Run the build
+    val runner = GradleRunner.create()
+    runner.forwardOutput()
+    runner.withPluginClasspath()
+    runner.withProjectDir(projectDir)
+    val result = runner.build()
 
-        // Verify the result
-        assertTrue(result.output.contains(Regex("My buildDir is .*out/aaos-apps-gradle-build")))
-    }
+    // Verify the result
+    assertTrue(result.output.contains(Regex("My buildDir is .*out/aaos-apps-gradle-build")))
+  }
 }
